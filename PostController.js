1514d40f3befe151a55fe508a59fbcbcf56f4766
Baraq-Code <it.agniev.a@gmail.com>
2021-12-11 19:04:46 +0200

    added controller, service, picture handling is not working due to express

--- PostController.js
+++ PostController.js
@@ -1,34 +1,31 @@
-import Post from './Post.js'
+import PostService from './PostService.js'
 
 class PostController {
+    //
     async create(req, res) {
         try {
-            const {author, title, content, picture} = req.body
-            const post = await Post.create({author, title, content, picture})
-            res.status(200).json(post)
+            console.log(req.files)
+            const post = await PostService.create(req.body)
+            res.json(post)
         }
         catch (e) {
-            res.status(500).json(e)
+            res.status(500).json(e.message)
         }
     }
 
     async getAll(req, res) {
         try {
-            const posts = await Post.find();
+            const posts = await PostService.getAll();
             return res.json(posts);
         }
         catch (e) {
-            res.status(500).json(e)
+            res.status(500).json(e.message)
         }
     }
-    
+
     async getOne(req, res) {
         try {
-            const {id} = req.params
-            if (!id) {
-                res.status(400).json({message: 'ID is not stated!!!'})
-            }
-            const post = await Post.findById(id);
+            const post = await PostService.getOne(req.params.id);
             return res.json(post);
         }
         catch (e) {
@@ -38,29 +35,21 @@ class PostController {
 
     async update(req, res) {
         try {
-            const post = req.body
-            if (!post._id) {
-                res.status(400).json({message: 'ID is not stated!!!'})
-            }
-            const updatedPost = await Post.findByIdAndUpdate(post._id, post, {new: true})
+            const updatedPost = await PostService.update(req.body)
             return res.json(updatedPost)
         }
         catch (e) {
-            res.status(500).json(e)
+            res.status(500).json(e.message)
         }
     }
 
     async delete(req, res) {
         try {
-            const {id} = req.params;
-            if (!id) {
-                res.status(400).json({message: 'ID is not stated!!!'})
-            }
-            const post = await Post.findByIdAndDelete(id)
+            const post = await PostService.delete(req.params.id)
             return res.json(post)
         }
         catch (e) {
-            res.status(500).json(e)
+            res.status(500).json(e.message)
         }
     }
 }
--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import router from './router.js'
+import fileUpload from 'express-fileupload'
 
 const PORT = 5000;
 const DB_URL = `mongodb+srv://admin:admin@cluster0.gfp4m.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const app = express();
 
 app.use(express.json());
+app.use(express.static('static'))
 app.use('/api', router);
+app.use(fileUpload({}))
 
 async function startApp() {
     try {
